df6592012a4fcffde9ea990af289f0e28f143b18
Caitlin-Breytenbach <25861409@sun.ac.za>
2026-05-14 10:05:49 +0200

    refactor: remove restart code

--- src/optimiser.jl
+++ src/optimiser.jl
@@ -6,23 +6,13 @@ export optimiser
 
 function optimiser(estimator, objective, initial, upper, lower )
     iter = Ref(0)
-    best_f = Ref(Inf)
-    best_x = copy(initial)
-    val = objective(initial)
-    x = initial
-
     function objective_fn(x_norm::Vector, grad::Vector)
-
-        if val < best_f[]
-            best_f[] = val 
-            best_x .= x 
-        end 
 
         x = @. x_norm*(upper - lower) + lower
         val = objective(x)
 
         iter[] += 1 
-        if iter[] % 10 == 0
+        if iter[] % 10 == 0 || iter[] == 1
             println("Iteration: $(iter[])")
             println("objective: $(round(val; digits = 6))")
             println("parameters: $(round.(x; sigdigits = 5))")
@@ -31,7 +21,7 @@ function optimiser(estimator, objective, initial, upper, lower )
         if length(grad)> 0
             f = x_norm -> objective(@. x_norm*(upper-lower) + lower)
             FiniteDiff.finite_difference_gradient!(grad, f, x_norm)
-            println("  Gradient      : $(round.(grad; sigdigits=3))")
+            # println("  Gradient      : $(round.(grad; sigdigits=3))")
         end
         return val
     end
@@ -56,30 +46,6 @@ function optimiser(estimator, objective, initial, upper, lower )
         solution status       : $ret
         """
     )
-
-    restart = 1
-    improvement = Inf
-    while min_f > 1e-2 && improvement > 1e-4
-        prev_f = min_f 
-        restart += 1
-    
-        println("Restart from best point")
-        min_f, min_xnorm, ret = run_opt(best_x)
-        println(
-            """
-            Run $restart
-            Iterations            : $(iter[])
-            objective value       : $min_f
-            solution status       : $ret
-            """
-        )
-        improvement = prev_f - min_f
-         
-    end
-
-    if min_f > 0.1 && improvement <= 1e-4
-        println("No improvement. Best objective $(best_f[])")
-    end
 
     min_x = @. min_xnorm*(upper - lower) + lower
     model_opt = Clapeyron.return_model(estimator, model, min_x)
